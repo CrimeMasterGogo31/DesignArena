040a42ac156b1f3d5bf8ddc78dd1f7a61a1ec998
Subhasish Pani <v-subass@microsoft.com>
2020-08-18 23:55:28 +0530

    Add project files.

---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace AbstractFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShapeFactory shapeFactory = new ShapeFactory();
+            var circleObj = shapeFactory.GetData((int)Shape.Circle).DataItem as Circle;
+            circleObj.GetInfo();
+
+        }
+    }
+
+    interface IFactory
+    {
+        FactoryItem GetData(int type);
+    }
+
+    class FactoryItem
+    {
+        public FactoryItem(object dataItem)
+        {
+            this._dataItem = dataItem;
+        }
+        private object _dataItem;
+        public object DataItem { get { return _dataItem; } }
+    }
+
+    abstract class AbstractFactory : IFactory
+    {
+        public abstract FactoryItem GetData(int type);
+
+    }
+
+    class ShapeFactory : AbstractFactory
+    {
+        public override FactoryItem GetData(int type)
+        {
+            FactoryItem factoryItem = null;
+            switch ((Shape)type)
+            {
+                case Shape.Circle:
+                    factoryItem = new FactoryItem(new Circle());
+                    break;
+            }
+
+            return factoryItem;
+        }
+    }
+
+    enum Shape
+    {
+        Circle = 1,
+        Square = 2,
+    }
+
+    public class Circle
+    {
+        public void GetInfo()
+        {
+            Console.WriteLine(" Circle CLass");
+        }
+    }
+
+    enum Color
+    {
+        Circle = 1,
+        Square = 2,
+    }
+
+    public class Red
+    {
+        public void GetInfo()
+        {
+            Console.WriteLine(" Red CLass");
+        }
+    }
+}
---
+++ ObserverPattern/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ObserverPattern
+{
+    // https://www.youtube.com/watch?v=oVAFvyICmbw
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var weatherStation = new WeatherStation();
+
+            var newsChannel1 = new NewsChannel("Independence TV");
+            weatherStation.AddObserver(newsChannel1);
+
+            var newsChannel2 = new NewsChannel("India Tomorrow");
+            weatherStation.AddObserver(newsChannel2);
+
+            weatherStation.Temperature = 10.2f;
+            weatherStation.Temperature = 23.2f;
+            weatherStation.Temperature = 45.2f;
+
+
+        }
+    }
+
+    public interface ISubject
+    {
+        void AddObserver(IObserver observer);
+        void RemoveObserver(IObserver observer);
+        void Notify();
+
+    }
+
+    public class WeatherStation : ISubject
+    {
+        List<IObserver> observers;
+        float _temp;
+        public float Temperature
+        {
+            get
+            {
+                return this._temp;
+            }
+
+            set
+            {
+                this._temp = value;
+                Notify();
+            }
+        }
+        public WeatherStation()
+        {
+            observers = new List<IObserver>();
+        }
+        public void AddObserver(IObserver observer)
+        {
+            this.observers.Add(observer);
+        }
+
+        public void Notify()
+        {
+            observers.ForEach(
+                o => o.Update(this));
+        }
+
+        public void RemoveObserver(IObserver observer)
+        {
+            this.observers.Remove(observer);
+        }
+    }
+
+    public interface IObserver
+    {
+        void Update(ISubject subject);
+    }
+
+    public class NewsChannel : IObserver
+    {
+        string name;
+        public NewsChannel(string name)
+        {
+            this.name = name;
+        }
+        public void Update(ISubject subject)
+        {
+            WeatherStation station = subject as WeatherStation;
+
+            if (station is WeatherStation)
+            {
+                Console.WriteLine("{0} News Channel has updated temprature as {1}", this.name, station.Temperature);
+            }
+
+        }
+    }
+}
