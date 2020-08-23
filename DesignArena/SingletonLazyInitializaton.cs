53502f211fb0a94a192b0aae34e5251fa74c5839
Subhasish Pani <subhasish.pani@microsoft.com>
2020-08-24 01:36:08 +0530

    - Open close design principle -  Lazy singleton pattern

---
+++ DesignArena/SingletonLazyInitializaton.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignArena
+{
+    public sealed class MySingleton
+    {
+        private MySingleton() { }
+        private static readonly Lazy<MySingleton> lazyObjecct = new Lazy<MySingleton>(() => new Lazy<MySingleton>);
+
+        public static MySingleton Instance { get { return lazyObjecct.Value; } }
+    }
+
+}
